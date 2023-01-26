eab76f48ccc92d65f8098f5af616bc47c0d0f08e
JasubiPL <gameportal7@hotmail.com>
2023-01-26 11:59:28 -0600

    if y else

---
+++ 06_if.py
@@ -0,0 +1,23 @@
+# import random
+
+# num = random.randint(0,1)
+
+# if num > 0.5:
+#     print('Heads')
+# else:
+#     print('Tails')
+
+grade = int(input("What grade did you have?: "))
+
+if grade > 90:
+    print("Your grade is A")
+elif grade > 80:
+        print("Your grade is A")
+elif grade > 70:
+        print("Your grade is A")
+elif grade > 60:
+        print("Your grade is D")
+else:
+    print("You failed")
+
+
---
+++ 07_modulo_random.py
@@ -0,0 +1,33 @@
+import random;
+
+num = random.randint(1,9)
+
+question = input("Whats is your question?: ")
+
+if num == 1:
+    print(f"Question: {question}")
+    print("Answer: Yes - definitely")
+elif num == 2:
+    print(f"Question: {question}")
+    print("Answer: It is decidedly so")
+elif num == 3:
+    print(f"Question: {question}")
+    print("Answer: Without a doubt")
+elif num == 4:
+    print(f"Question: {question}")
+    print("Answer: Reply hazy, try again")
+elif num == 5:
+    print(f"Question: {question}")
+    print("Answer: Ask again later")
+elif num == 6:
+    print(f"Question: {question}")
+    print("Answer: Better not tell you now")
+elif num == 7:
+    print(f"Question: {question}")
+    print("Answer: My sources say no")
+elif num == 8:
+    print(f"Question: {question}")
+    print("Answer: Outlook not so good")
+elif num == 9:
+    print(f"Question: {question}")
+    print("Answer: Very doubtful")
