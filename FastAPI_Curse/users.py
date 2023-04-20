12d2ecaa7f1e164766393e429fd70c455fab7984
JasubiPL <gameportal7@hotmail.com>
2023-04-20 16:50:42 -0600

    Add query path

--- FastAPI_Curse/users.py
+++ FastAPI_Curse/users.py
@@ -5,16 +5,16 @@ app = FastAPI()
 
 #Entidad
 class User(BaseModel):
-  name: str
+  id:int
   surname: str
   url: str
   age: int
 
 #Lista de usuarios creados como entidad del objeto User
-user_list = [User(name="Juan", surname="Perez", url="https://www.google.com", age=30),
-             User(name="Carlos", surname="Perez", url="https://www.google.com", age=35),
-             User(name="Jose", surname="Perez", url="https://www.google.com", age=20),
-             User(name="Pedro", surname="Perez", url="https://www.google.com", age=30)]
+user_list = [User(id=4,name="Juan", surname="Perez", url="https://www.google.com", age=30),
+             User(id=3,name="Carlos", surname="Perez", url="https://www.google.com", age=35),
+             User(id=2,name="Jose", surname="Perez", url="https://www.google.com", age=20),
+             User(id=1,name="Pedro", surname="Perez", url="https://www.google.com", age=30)]
 
 @app.get("/usersjson")
 async def usersjson():
@@ -27,3 +27,19 @@ async def usersjson():
 async def usersjson():
   return user_list
 
+
+@app.get("/user/{id}")
+async def user(id:int):
+  return searchUsers(id)
+  
+@app.get("/user/")
+async def user(id:int):
+  return searchUsers(id)
+
+def searchUsers(id):
+  users = filter(lambda user:user.id == id, user_list)
+  try:
+    return list(users)[0]
+  except:
+    return {"err": "No se ha encontrado usuario"}
+  
