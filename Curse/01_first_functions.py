58e9ee87eb4e9c639b54ec85dc8db4d2ce48f5d6
JasubiPL <gameportal7@hotmail.com>
2023-12-06 10:40:40 -0600

    Functions with Return

--- Curse/01_first_functions.py
+++ Curse/01_first_functions.py
@@ -1,5 +1,23 @@
+#funciones sin Parametros
 def mensaje():
   print("Mi primer ejersicio")
 
-mensaje()
-mensaje()
+#mensaje()
+#mensaje()
+
+#funciones con Parametros
+def suma(n1, n2):
+  print(n1+n2)
+
+#suma(3,4)
+#suma(8,23)
+
+#funciones con Retorno
+def suma2(n1, n2):
+  resultado = n1+n2
+  return resultado
+
+almacena_resultado = suma2(5,8)
+
+print(almacena_resultado)
+print(suma2(3,4))
